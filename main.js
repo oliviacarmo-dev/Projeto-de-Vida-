73b66abd91881b5b7ab9f4be14a7982bd7cf4730
oliviacarmo-dev <olivia.carmo@escola.pr.gov.br>
2026-06-13 20:36:25 +0000

    Aula 5

--- main.js
+++ main.js
@@ -14,7 +14,43 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2026-12-01T00:00:00");
+const tempoObjetivo2 = new Date("2027-12-01T00:00:00");
+const tempoObjetivo3 = new Date("2028-12-01T00:00:00");
+const tempoObjetivo4 = new Date("2029-12-01T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+  
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1); //Antes
+
+contadores[0].textContent = calculaTempo(tempos[0]); // Depois
+
+
+
+
+}
